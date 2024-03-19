8edcfd2aeb3c8a54ff00f086e7f33863eee9579b
midir99 <midir99@proton.me>
2024-03-19 17:21:47 -0600

    Add table and search capabilities to search people page

--- abuelitos/civilizations/forms.py
+++ abuelitos/civilizations/forms.py
@@ -1,23 +1,27 @@
+import datetime
+
 from django import forms
 from django.core.exceptions import ValidationError
+from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
+from django.utils import timezone
 from . import choices
 
 
 class PeopleSearchForm(forms.Form):
-    full_name = forms.CharField(
+    full_name_or_alias = forms.CharField(
         required=False,
         max_length=200,
         min_length=1,
     )
     year_of_birth = forms.IntegerField(
         required=False,
-        min_value=0,
+        min_value=1,
     )
     year_of_death = forms.IntegerField(
         required=False,
-        min_value=0,
+        min_value=1,
     )
     agee_code = forms.ChoiceField(
         required=False,
@@ -31,6 +35,26 @@ class PeopleSearchForm(forms.Form):
         required=False,
         max_length=4,
     )
+
+    def clean_year_of_birth(self):
+        year_of_birth = self.cleaned_data["year_of_birth"]
+        if year_of_birth is None:
+            return year_of_birth
+        tzinfo = timezone.get_current_timezone() if settings.USE_TZ else None
+        current_year = datetime.datetime.now(tz=tzinfo).year
+        if year_of_birth > current_year:
+            raise ValidationError(_("The year of birth cannot be later than %(current_year)s." % {"current_year": current_year}))
+        return year_of_birth
+
+    def clean_year_of_death(self):
+        year_of_death = self.cleaned_data["year_of_death"]
+        if year_of_death is None:
+            return year_of_death
+        tzinfo = timezone.get_current_timezone() if settings.USE_TZ else None
+        current_year = datetime.datetime.now(tz=tzinfo).year
+        if year_of_death > current_year:
+            raise ValidationError(_("The year of death cannot be later than %(current_year)s." % {"current_year": current_year}))
+        return year_of_death
 
     def clean(self):
         cleaned_data = super().clean()
--- abuelitos/civilizations/views.py
+++ abuelitos/civilizations/views.py
@@ -1,17 +1,22 @@
-from django.views import generic as generic_views
+import datetime
+
+from django.views import generic as views_generic
+from django.db.models import Q
 
 from . import choices, forms, models
 
 
-class HomeView(generic_views.TemplateView):
+class HomeView(views_generic.TemplateView):
     template_name = "th_olian/base.html"
 
 
 home_view = HomeView.as_view()
 
 
-class PeopleSearchView(generic_views.TemplateView):
+class PeopleSearchView(views_generic.ListView):
     template_name = "civilizations/people_search_form.html"
+    model = models.Person
+    paginate_by = 1
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -29,30 +34,58 @@ class PeopleSearchView(generic_views.TemplateView):
                 }
                 for state in choices.AGEECode
             ]
+            context["options_for_agem_code"] = (
+                models.Locality.objects.get_municipalities_of_state(agee_code)
+            )
             agem_code = self.request.GET.get("agem_code")
             if agem_code:
-                context["options_for_agem_code"] = (
-                    models.Locality.objects.get_municipalities_of_state(agee_code)
-                )
                 for m in context["options_for_agem_code"]:
                     m["is_selected"] = m["agem_code"] == agem_code
+                context["options_for_loc_code"] = (
+                    models.Locality.objects.get_localities_of_municipality(
+                        agee_code, agem_code
+                    )
+                )
                 loc_code = self.request.GET.get("loc_code")
                 if loc_code:
-                    context["options_for_loc_code"] = (
-                        models.Locality.objects.get_localities_of_municipality(
-                            agee_code, agem_code
-                        )
-                    )
                     for l in context["options_for_loc_code"]:
                         l["is_selected"] = l["loc_code"] == loc_code
-
         return context
+
+    def get_queryset(self):
+        queryset = models.Person.objects.all()
+        form = forms.PeopleSearchForm(self.request.GET)
+        if not form.is_valid():
+            return queryset
+        full_name_or_alias = form.cleaned_data["full_name_or_alias"]
+        if full_name_or_alias:
+            queryset = queryset.filter(Q(full_name__icontains=full_name_or_alias) | Q(alias__icontains=full_name_or_alias))
+        year_of_birth = form.cleaned_data["year_of_birth"]
+        if year_of_birth is not None:
+            date_start = datetime.date(year_of_birth - 5, 1, 1) if year_of_birth >= 5 else datetime.date(1, 1, 1)
+            date_end = datetime.date(year_of_birth + 5, 12, 31)
+            queryset = queryset.filter(date_of_birth__range=(date_start, date_end))
+        year_of_death = form.cleaned_data["year_of_death"]
+        if year_of_death is not None:
+            date_start = datetime.date(year_of_death - 5, 1, 1) if year_of_death >= 5 else datetime.date(1, 1, 1)
+            date_end = datetime.date(year_of_death + 5, 12, 31)
+            queryset = queryset.filter(date_of_death__range=(date_start, date_end))
+        agee_code = form.cleaned_data["agee_code"]
+        if agee_code:
+            queryset = queryset.filter(locality__agee_code=agee_code)
+        agem_code = form.cleaned_data["agem_code"]
+        if agem_code:
+            queryset = queryset.filter(locality__agem_code=agem_code)
+        loc_code = form.cleaned_data["loc_code"]
+        if loc_code:
+            queryset = queryset.filter(locality__loc_code=loc_code)
+        return queryset
 
 
 people_search_view = PeopleSearchView.as_view()
 
 
-class OptionsForAGEMCodeView(generic_views.TemplateView):
+class OptionsForAGEMCodeView(views_generic.TemplateView):
     template_name = "civilizations/options_for_agem_code.html"
 
     def get_context_data(self, **kwargs):
@@ -69,7 +102,7 @@ class OptionsForAGEMCodeView(generic_views.TemplateView):
 options_for_agem_code_view = OptionsForAGEMCodeView.as_view()
 
 
-class OptionsForLocCodeView(generic_views.TemplateView):
+class OptionsForLocCodeView(views_generic.TemplateView):
     template_name = "civilizations/options_for_loc_code.html"
 
     def get_context_data(self, **kwargs):
